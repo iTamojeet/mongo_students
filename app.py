c1eea46657589d120fde92254fcd372583aa308a
iTamojeet <bigcodingdinosaur@gmail.com>
2025-08-04 11:52:02 +0530

    conert to web application

--- app.py
+++ app.py
@@ -1,95 +1,71 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, redirect, url_for
 from pymongo import MongoClient
 from bson import ObjectId
 
-# Initialize Flask app
 app = Flask(__name__)
 
-# Connect to MongoDB Atlas
+# Connect to MongoDB
 client = MongoClient("mongodb+srv://admin:989144@clusterx.mke2gdh.mongodb.net/?retryWrites=true&w=majority&appName=ClusterX")
-db = client["school"]  # Use database named "school"
-students = db["students"]  # Collection named "students"
+db = client["school"]
+students = db["students"]
 
-# Home route (renders HTML page)
 @app.route('/')
 def home():
     return render_template("home.html")
 
-# ----------------------------- #
-#         CREATE STUDENT        #
-# ----------------------------- #
+@app.route('/form')
+def form_page():
+    return render_template("form.html")
+
+@app.route('/view')
+def view_students():
+    all_students = list(students.find())
+    for s in all_students:
+        s['_id'] = str(s['_id'])
+    return render_template("view.html", students=all_students)
+
 @app.route("/create", methods=["POST"])
 def create_student():
-    # Check if data is sent as JSON
-    if request.is_json:
-        body = request.get_json()
-
-        # Prepare data dictionary with nested "subjects"
-        data = {
-            "name": body.get("name"),
-            "address": body.get("address"),
-            "email": body.get("email"),
-            "subjects": {
-                "name": body.get("subjects", {}).get("name"),
-                "author": body.get("subjects", {}).get("author"),
-                "description": body.get("subjects", {}).get("description")
-            }
+    form = request.form
+    data = {
+        "name": form.get("name"),
+        "address": form.get("address"),
+        "email": form.get("email"),
+        "subjects": {
+            "name": form.get("subject_name"),
+            "author": form.get("subject_author"),
+            "description": form.get("subject_description")
         }
+    }
+    students.insert_one(data)
+    return redirect(url_for('view_students'))
 
-        print("Data to insert:", data)
-        students.insert_one(data)  # Insert into MongoDB
-        return "Student inserted successfully via JSON!"
-
-    else:
-        # If data is submitted via HTML form
-        form = request.form
-        data = {
-            "name": form.get("name"),
-            "address": form.get("address"),
-            "email": form.get("email"),
-            "subjects": {
-                "name": form.get("subject_name"),
-                "author": form.get("subject_author"),
-                "description": form.get("subject_description")
-            }
-        }
-
-        students.insert_one(data)
-        return "Student inserted successfully via form!"
-
-# ----------------------------- #
-#         READ STUDENTS         #
-# ----------------------------- #
-@app.route('/view', methods=['GET'])
-def read_students():
-    result = []
-    for s in students.find():
-        s['_id'] = str(s['_id'])  # Convert ObjectId to string for JSON
-        result.append(s)
-    return jsonify(result)  # Return all student records as JSON
-
-# ----------------------------- #
-#         UPDATE STUDENT        #
-# ----------------------------- #
-@app.route('/update/<id>', methods=['PUT'])
-def update_student(id):
-    data = request.json  # Get update fields from request body
-    students.update_one(
-        {"_id": ObjectId(id)},
-        {"$set": data}
-    )
-    return jsonify({"msg": "Student updated."})
-
-# ----------------------------- #
-#         DELETE STUDENT        #
-# ----------------------------- #
-@app.route('/delete/<id>', methods=['DELETE'])
+@app.route('/delete/<id>')
 def delete_student(id):
     students.delete_one({"_id": ObjectId(id)})
-    return jsonify({"msg": "Student deleted."})
+    return redirect(url_for('view_students'))
 
-# ----------------------------- #
-#         RUN THE SERVER        #
-# ----------------------------- #
+@app.route('/edit/<id>')
+def edit_page(id):
+    student = students.find_one({"_id": ObjectId(id)})
+    student['_id'] = str(student['_id'])
+    return render_template("edit.html", student=student)
+
+@app.route('/update/<id>', methods=['POST'])
+def update_student(id):
+    form = request.form
+    updated_data = {
+        "name": form.get("name"),
+        "address": form.get("address"),
+        "email": form.get("email"),
+        "subjects": {
+            "name": form.get("subject_name"),
+            "author": form.get("subject_author"),
+            "description": form.get("subject_description")
+        }
+    }
+    students.update_one({"_id": ObjectId(id)}, {"$set": updated_data})
+    return redirect(url_for('view_students'))
+
 if __name__ == "__main__":
     app.run(debug=True)
